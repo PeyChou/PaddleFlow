54fe5e723e2b875d4f4e569874ce25d88a954e51
PeyChou <peychou@qq.com>
2024-04-24 14:59:57 +0800

    新增后台运行模式，更新go.mod

---
+++ cmd/fs/fuse/service/mount_test.go
@@ -0,0 +1,72 @@
+package service
+
+import (
+	"fmt"
+
+	"github.com/PaddlePaddle/PaddleFlow/pkg/fs/utils"
+	"github.com/PaddlePaddle/PaddleFlow/pkg/version"
+	"github.com/urfave/cli/v2"
+	"os"
+	"testing"
+	"time"
+)
+
+const testMountPoint = "./mock"
+
+func MockMain(args []string) error {
+	cli.VersionFlag = &cli.BoolFlag{
+		Name: "version", Aliases: []string{"V"},
+		Usage: "print only the version",
+	}
+	app := &cli.App{
+		Name:                 "pfs-fuse",
+		Usage:                "A POSIX file system built on kv DB and object storage.",
+		Version:              version.InfoStr(),
+		Copyright:            "Apache License 2.0",
+		HideHelpCommand:      true,
+		EnableBashCompletion: true,
+		Commands: []*cli.Command{
+			CmdMount(),
+			CmdUmount(),
+			CmdStats(),
+			CmdBench(),
+			CmdWarmup(),
+		},
+	}
+	return app.Run(args)
+}
+
+func mountTemp(t *testing.T) {
+	os.MkdirAll(testMountPoint, 0755)
+	mountArgs := []string{"", "mount", "-d", "true"}
+	go func() {
+		if err := MockMain(mountArgs); err != nil {
+			t.Errorf("mount failed: %s", err)
+		}
+	}()
+	time.Sleep(3 * time.Second)
+	inode, err := utils.GetFileInode(testMountPoint)
+	if err != nil {
+		t.Fatalf("get file inode failed: %s", err)
+	}
+	if inode != 1 {
+		t.Fatalf("mount failed: inode of %s is not 1", testMountPoint)
+	} else {
+		t.Logf("mount %s success", testMountPoint)
+	}
+}
+
+func umountTemp(t *testing.T) {
+	if err := MockMain([]string{"", "umount", testMountPoint}); err != nil {
+		t.Fatalf("umount failed: %s", err)
+	}
+}
+
+func TestMountBackground(t *testing.T) {
+	mountTemp(t)
+	defer umountTemp(t)
+
+	if err := os.WriteFile(fmt.Sprintf("%s/f1.txt", testMountPoint), []byte("test"), 0644); err != nil {
+		t.Fatalf("write file failed: %s", err)
+	}
+}
